ce5da5f9e25a8e42597b49223c752aca6828ec53
Innocience <emailzeichen@web.de>
2019-08-28 21:00:48 +0200

    Added animations and fixed related code

--- assets_src/lua/initialized/leveling.lua
+++ assets_src/lua/initialized/leveling.lua
@@ -16,7 +16,7 @@ function Leveling.init()
     --        Leveling.loadStates(unit)
     --    end
     --end    
-end                        
+end
 --################--
 
 --### CONSTS ###--
@@ -25,23 +25,26 @@ local rankCount = 5
 -- absolute summand added to the total exp requirements for rankup
 --local rankAbsolutes = {0, 0, 0, 0, 0}
 -- multiplier for cost added to the total exp requirements for rankup
-local rankCostMult = { [0] = 0.0, 
+local rankCostMult = { 
+                       [0] = 0.0, 
                        [1] = 1.0, 
                        [2] = 3.0, 
                        [3] = 6.0, 
                        [4] = 10.0,
                        [5] = 15.0
-                       }
+                     }
 -- multipliers for effectiveness of units
-local rankOffMult = { [0] = 1.0, 
+local rankOffMult = { 
+                      [0] = 1.0, 
                       [1] = 1.1, 
                       [2] = 1.2, 
                       [3] = 1.3, 
                       [4] = 1.4,
                       [5] = 1.5
-                      }                       
+                    }
 -- disallowed classes from leveling
-local canLevelTable = { commander = false, 
+local canLevelTable = { 
+                        commander = false, 
                         soldier = true, 
                         spearman = true,
                         dog = true,
@@ -65,7 +68,7 @@ local canLevelTable = { commander = false,
                         warship = true,
                         
                         fieldcom = true
-                        }
+                      }
 --##############--
 
 --### LOADING ###--
@@ -146,21 +149,21 @@ end
 -- Call to erase and redraw rank sprite after combat
 -- Provide gained exp if wanna combine with adding exp
 function Leveling.update(unit, gained_exp)
-    if Leveling.canLevel(unit) then
-        gained_exp = gained_exp or 0
-        Leveling.addExperience(unit, gained_exp)
+    gained_exp = gained_exp or 0
+    if Leveling.canLevel(unit) then        
+        local rankGain = Leveling.addExperience(unit, gained_exp)
         Leveling.updateGrooveUI(unit)
-        Leveling.deleteRankSprite(unit)
-        Leveling.drawRankSprite(unit)
+        Leveling.drawRankSprite(unit, rankGain)
         Wargroove.updateUnit(unit)
     end
 end
 
 -- Adds a value to the units experience and updates rank
+-- returns rank difference
 function Leveling.addExperience(unit, value)
     local experience = Leveling.getExperience(unit) or 0
     Leveling.setExperience(unit, experience + value)
-    Leveling.updateRank(unit)
+    return Leveling.updateRank(unit)
 end
 
 -- returns an 0-index based array of total required exp for rank
@@ -168,26 +171,28 @@ end
 function Leveling.getRankExpReqs(unit)
     local cost = unit.unitClass.cost
     
-    local retval = {}
-    for i = 0, rankCount do
+    local retval = { [0] = 0}
+    for i = 1, rankCount do
         retval[i] = rankCostMult[i] * cost --rankAbsolutes[i] + rankCostMult[i] * cost
     end
     
     return retval
 end
 
--- returns rank
+-- returns rank difference
 function Leveling.updateRank(unit)    
     local experience = Leveling.getExperience(unit) or 0
     local expReqs = Leveling.getRankExpReqs(unit)
-
-    for i=0, rankCount do
+    local oldRank = Leveling.getRank(unit) or 0
+    
+    for i=1, rankCount do
         if tonumber(experience) < expReqs[i] then             
             Wargroove.setUnitState(unit, "rank", i - 1)
-            return
+            return i - 1 - tonumber(oldRank)
         end
     end
     Wargroove.setUnitState(unit, "rank", rankCount)
+    return rankCount - tonumber(oldRank)
 end
 
 --############--
@@ -207,27 +212,35 @@ end
 
 
 --### UI ###--
-local rankSprites = { "fx/ranks/rank1",
+local rankSprites = { 
+                      "fx/ranks/rank1",
                       "fx/ranks/rank2",
                       "fx/ranks/rank3",
                       "fx/ranks/rank4",
                       "fx/ranks/rank5"
                     }
                     
--- Adds the unit effect to display rank of a unit
-function Leveling.drawRankSprite(unit)
-    local rank = tonumber(Leveling.getRank(unit)) or 0    
-    if rank > 0 then
-        Leveling.setRankSpriteId(unit, Wargroove.spawnUnitEffect(unit.id, rankSprites[rank], "idle", "spawn", true))
-    end
-end
-
--- Removes the associated rank_sprite from unit
-function Leveling.deleteRankSprite(unit)
+-- Adds the unit effect to display rank of a unit and deletes old sprite
+function Leveling.drawRankSprite(unit, rankGain)
+    rankGain = rankGain or 0
+    
+    -- sprite deletion
     local rank_sprite = Leveling.getRankSpriteId(unit)
     if rank_sprite ~= nil and rank_sprite ~= "" then
         Wargroove.deleteUnitEffect(rank_sprite, "death")       
         Leveling.setRankSpriteId(unit, "")
+    end
+    
+    -- sprite drawing
+    local rank = tonumber(Leveling.getRank(unit)) or 0
+    if rank > 0 then
+        if rankGain > 0 then
+            Leveling.setRankSpriteId(unit, Wargroove.spawnUnitEffect(unit.id, rankSprites[rank], "idle", "rankup", true))
+        elseif rankGain == 0 then
+            Leveling.setRankSpriteId(unit, Wargroove.spawnUnitEffect(unit.id, rankSprites[rank], "idle", "idle", true))
+        else
+            Leveling.setRankSpriteId(unit, Wargroove.spawnUnitEffect(unit.id, rankSprites[rank], "idle", "rankdown", true))
+        end
     end
 end
 
